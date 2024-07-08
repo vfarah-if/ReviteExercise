d8c5971bec6a9f50bbfab53812624f7bea208368
Vincent Farah <vincent.farah@newlook.com>
2024-07-08 07:21:55 +0100

    Update react linting

---
+++ .eslintrc.cjs
@@ -0,0 +1,33 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'plugin:prettier/recommended',
+    'plugin:jest/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
+  ],
+  env: {
+    'jest/globals': true,
+    browser: true,
+    es2020: true,
+  },
+  ignorePatterns: ['dist', 'eslintrc.cjs'],
+  plugins: ['react-refresh'],
+  settings: {
+    react: {
+      version: 18.2,
+    },
+  },
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  rules: {
+    'react/jsx-no-target-blank': 'off',
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+  },
+}
