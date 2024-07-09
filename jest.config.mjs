16f12b8747ca562eaec6995f248ab531f828b9e4
Vincent Farah <vincent.farah@newlook.com>
2024-07-10 00:42:53 +0100

    Created and configured the button tests Removed the sum tests Tweaked jest to mock CSS Changed jest to work with browser environment Finally got react test to work

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,9 +1,34 @@
 export default {
   preset: 'ts-jest',
-  testEnvironment: 'node',
+  testEnvironment: 'jsdom',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   transform: {
     '^.+\\.tsx?$': 'ts-jest',
   },
-  testMatch: ['**/*.test.ts'],
+  testMatch: ['**/*.test.ts', '**/*.test.tsx'],
+  moduleNameMapper: {
+    '\\.css$': 'identity-obj-proxy',
+  },
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  collectCoverage: true, 
+  collectCoverageFrom: [
+    'src/**/*.{js,jsx,ts,tsx}', 
+    '!src/**/*.d.ts', 
+	'!src/**/*.test.ts', 
+	'!src/**/*.test.tsx', 
+	'!src/**/*.stories.ts', 
+	'!src/**/index.ts', 
+  ],
+  coverageDirectory: 'coverage', 
+  coverageReporters: ['text', 'lcov'],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
 }
---
+++ src/components/Button/Button.test.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { fireEvent, render, screen } from '@testing-library/react'
+import '@testing-library/jest-dom'
+import { Button } from './Button'
+
+describe('Button should', () => {
+
+	test('render button with label', () => {
+		render(<Button label="Click Me" />)
+		const buttonElement = screen.getByText(/Click Me/i)
+		expect(buttonElement).toBeInTheDocument()
+	  });	 
+	  
+	  test('apply primary class when primary prop is true', () => {
+		render(<Button label="Click Me" primary />)
+		const buttonElement = screen.getByText(/Click Me/i)
+		expect(buttonElement).toHaveClass('storybook-button--primary')
+	  });
+
+	  test('apply size class correctly', () => {
+		render(<Button label="Click Me" size="large" />)
+		const buttonElement = screen.getByText(/Click Me/i)
+		expect(buttonElement).toHaveClass('storybook-button--large')
+	  });
+
+	  test('applies background color style correctly', () => {
+		const backgroundColor = 'red'
+		render(<Button label="Click Me" backgroundColor={backgroundColor} />)
+		const buttonElement = screen.getByText(/Click Me/i)
+		expect(buttonElement).toHaveStyle(`background-color: ${backgroundColor}`)
+	  });
+
+	  test('calls onClick handler when clicked', () => {
+		const handleClick = jest.fn()
+		render(<Button label="Click Me" onClick={handleClick} />)
+		const buttonElement = screen.getByText(/Click Me/i)
+		fireEvent.click(buttonElement)
+		expect(handleClick).toHaveBeenCalledTimes(1)
+	  });
+});
